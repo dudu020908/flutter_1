8ca9ef89124808626f25785159d23db35a37d251
dudu020908 <lygdleh@naver.com>
2025-03-27 12:39:20 +0900

    20250327

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:daelim_2025/presentation/My_App.dart';
+import 'package:daelim_2025/presentation/my_App.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _count = 0;
+  void _handleIncreaseCount() => setState(() {
+    _count++;
+  });
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 99, 255, 234),
+        title: Text('FlutterDemo'),
+      ),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('버튼 클릭 시 횟수 증가', style: TextStyle(fontSize: 34)),
+              Text('$_count', style: TextStyle(fontSize: 50)),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _handleIncreaseCount,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
